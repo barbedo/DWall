2ae2df2553a9ef677af706ef4898e28ece1ffdd6
Cgroup <contactclient@gmail.com>
2016-01-21 00:11:30 +0100

    Add EditActivity layout

---
+++ app/src/main/java/com/barbedo/dwall/activities/EditActivity.java
@@ -0,0 +1,15 @@
+package com.barbedo.dwall.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.barbedo.dwall.R;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
